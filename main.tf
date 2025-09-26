aa3ca80074a85b460b223c27273596857a1c6bf4
jbsparkdevops <bharghav588@gmail.com>
2025-09-25 20:10:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,86 @@
 provider "google" {
-  credentials = jsondecode(var.credentials_json)
-  project     = var.project
-  region      = var.region
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-vm2"
-  machine_type = "e2-micro"
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
+    auto_delete = true
+    device_name = var.instance_name
+
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.boot_image
+      size  = 10
+      type  = "pd-balanced"
     }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src           = "vm_add-tf"
+    goog-ops-agent-policy = var.ops_agent_policy
+  }
+
+  metadata = {
+    enable-osconfig = "TRUE"
   }
 
   network_interface {
-    network       = "default"
-    access_config {}
+    subnetwork  = var.subnetwork
+    stack_type  = "IPV4_ONLY"
+    queue_count = 0
+
+    access_config {
+      network_tier = "PREMIUM"
+    }
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = var.scopes
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+}
+
+module "ops_agent_policy" {
+  source        = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
+  project       = var.project_id
+  zone          = var.zone
+  assignment_id = "${var.ops_agent_policy}-${var.zone}"
+
+  agents_rule = {
+    package_state = "installed"
+    version       = "latest"
+  }
+
+  instance_filter = {
+    all = false
+    inclusion_labels = [{
+      labels = {
+        goog-ops-agent-policy = var.ops_agent_policy
+      }
+    }]
   }
 }
